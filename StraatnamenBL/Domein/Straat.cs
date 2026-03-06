e565f0e292e9c9fede48771f1bac39658eb8187e
Mouhannad3 <233546700+Mouhannad3@users.noreply.github.com>
2026-03-06 10:26:40 +0100

    Add project files.

---
+++ StraatnamenBL/Domein/Straat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StraatnamenBL.Domein
+{
+    public class Straat
+    {
+        public string Naam { get; set; }
+        public Gemeente Gemeente { get; set; }
+
+
+    }
+}
---
+++ StraatnamenUI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StraatnamenUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
